937e3e4aa954da302b360966f444e02f1a43d3ba
Richard Rex <turquoisedragon2926@gmail.com>
2024-10-31 07:29:47 -0700

    plot_vel

--- scripts/plot_prior.jl
+++ scripts/plot_prior.jl
@@ -2,26 +2,33 @@
 using DrWatson
 using JLD2
 using PyPlot, SlimPlotting
+using Images
 
 include("CNF/utils.jl")
 include("config.jl")
 
 params = Config.get_parameters()
+down_rate = params["down_rate"]
+
+function plot_vel(x, filename; smooth=false, gaussian=20)
+    if smooth
+        x = x_smooth = 1f0./Float32.(imfilter(1f0./x[:, :, 1, 1], Kernel.gaussian(gaussian)))
+    end
+
+    imshow(x, vmin=1.5, vmax=4.8,  interpolation="none", cmap="cet_rainbow4")
+    savefig(filename, bbox_inches="tight", dpi=300)
+
+    close()
+end
+
 @load "data/posteriors_iteration_j=0.jld2" x0
+x = x0
+
+@load "data/synthoseis_no_salt.jld2" data
+x = data[1:down_rate:end, 1:down_rate:end, :, :]
 
 # @load "data/initial.jld2" x
-# x0 = x
 
-gaussian = 20
-
-imshow(x0[:, :, 1, 1]', vmin=minimum(x0), vmax=maximum(x0),  interpolation="none", cmap="cet_rainbow4")
-savefig("plots/velocity_1.png", bbox_inches="tight", dpi=300)
-
-close()
-
-x_smooth = 1f0./Float32.(imfilter(1f0./x0[:, :, 1, 1], Kernel.gaussian(gaussian)))
-
-imshow(x_smooth', vmin=minimum(x0), vmax=maximum(x0),  interpolation="none", cmap="cet_rainbow4")
-savefig("plots/velocity_smooth_1.png", bbox_inches="tight", dpi=300)
-
-close()
+plot_vel(x[:, :, 1, 1]', "plots/no_salt.png")
+plot_vel(x0[:, :, 1, 1]', "plots/no_salt_smoothed.png")
+plot_vel(x[:, :, 1, 1]', "plots/no_salt_smoothed_true.png", smooth=true)
